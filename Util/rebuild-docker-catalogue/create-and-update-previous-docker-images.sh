95bbd99acd2bd5d692a72f7d8d5959736186c0e7
Alan Roth <alan.roth@payara.fish>
2020-10-16 16:13:24 +0100

    Added a regex check to check file WIP

--- Util/rebuild-docker-catalogue/create-and-update-previous-docker-images.sh
+++ Util/rebuild-docker-catalogue/create-and-update-previous-docker-images.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+#cd into script location
+cd $(dirname $BASH_SOURCE)
+
 REPO_DIR="./payara-enterprise"
 
 #Change to HTTPS if running locally
-ENTERPRISE_REMOTE="git@github.com:payara/payara-enterprise.git"
+ENTERPRISE_REMOTE="https://github.com/AlanRoth/payara-enterprise"
 
 if [ ! -f payara.versions ]; then
     echo "payara.versions file is missing!"
+    exit -1
+fi
+
+#Should return 0. All payara versions should match the regex
+invalidVersions=$(grep -P -v -c "(5|4).(1.2.191|\d{2}).(\d+)" payara.versions)
+if [ ! ${invalidVersions} == 0 ]; then
+    echo "There is a invalid version in payara.versions"
+    #Return invalid versions
+    echo $(grep -P -v "(5|4).(1.2.191|\d{2}).(\d+)" payara.versions)
     exit -1
 fi
 
@@ -18,14 +30,17 @@ fi
 #Map payara versions to a local array
 mapfile -t versionArray < payara.versions
 
+#Sort array
+sortedVersionArray=( $( printf "%s\n" "${versionArray[@]}" | sort -nr ) )
+
 cd ${REPO_DIR}
 
-#For loop for every version in version array
-for i in "${versionArray[@]}"
-do
-    #Checkout master
-    git checkout master
+#Payara 5+ will be processed first in the sorted list
+git checkout master
 
+#For loop for every version in version array
+for i in "${sortedVersionArray[@]}"
+do
     #Check out maintenance if payara4
     if [ ${i:0:1} == "4" ]; then
         git checkout payara-server-4.1.2.191.maintenance
